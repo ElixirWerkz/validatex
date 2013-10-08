2d6669b073992ca74fddf49a3f84159a502a860c
Yurii Rashkovskii <yrashk@spawngrid.com>
2013-10-08 09:35:53 -0700

    Add the bang form of validate (validate!/1 and /2)

--- lib/validatex.ex
+++ lib/validatex.ex
@@ -1,5 +1,18 @@
 defmodule Validatex do
 
+  defexception ValidationFailure, name: nil, value: nil, validation: nil, error: nil do
+    def message(__MODULE__[name: name, value: value, validation: validation, error: error]) do
+      "Validation error on #{name}, #{inspect value} does not pass the validation " <>
+      "of #{inspect validation} with a reason of #{inspect error}"
+    end
+  end
+
+  defexception MultipleValidationFailures, failures: [] do
+    def message(__MODULE__[failures: failures]) do
+      Enum.join((lc failure inlist failures, do: failure.message), "\n")
+    end
+  end
+
   def validate(plan) do
       results = lc {name, value, spec} inlist plan do
         {name, value, spec, Validatex.Validate.valid?(spec, value)}
@@ -9,6 +22,22 @@ defmodule Validatex do
           _ -> true
       end
       Enum.filter results, only_errors
+  end
+
+  def validate!(plan, options // [report_all_errors: false]) do
+    case validate(plan) do
+      [] -> true
+      [{name, value, validation, error}|_] = errors ->
+        if options[:report_all_errors] do
+          exceptions =
+          lc {name, value, validation, error} inlist errors do
+            ValidationFailure.new(name: name, value: value, validation: validation, error: error)
+          end
+          raise MultipleValidationFailures, failures: exceptions
+        else
+          raise ValidationFailure, name: name, value: value, validation: validation, error: error
+        end
+    end
   end
 
   defrecord Numericality,
--- test/sheet_test.exs
+++ test/sheet_test.exs
@@ -7,11 +7,34 @@ defmodule Validatex.SheetTest do
     assert V.validate(plan) == []
   end
 
+  test :no_errors_bang do
+    plan = [{"age", "30", V.Numericality.new(allow_string: true)}]
+    assert V.validate!(plan) == true
+  end
+
   test :errors do
     plan = [{"age", "30", V.Numericality.new(allow_string: false)},
             {"age", 30, V.Range.new(to: 18)}
            ]
     assert length(V.validate(plan)) == 2
+  end
+
+  test :errors_bang do
+    plan = [{"age", "30", V.Numericality.new(allow_string: false)},
+            {"age", 30, V.Range.new(to: 18)}
+           ]
+    assert_raise Validatex.ValidationFailure, "Validation error on age, \"30\" does not pass the validation " <>
+                                              "of #{inspect V.Numericality.new(allow_string: false)} with a reason of :string_not_allowed", fn ->
+      V.validate!(plan)
+    end
+    assert_raise Validatex.MultipleValidationFailures,
+        "Validation error on age, \"30\" does not pass the validation " <>
+        "of #{inspect V.Numericality.new(allow_string: false)} with a reason of :string_not_allowed\n" <>
+        "Validation error on age, 30 does not pass the validation " <>
+        "of #{inspect V.Range.new(to: 18)} with a reason of :greater", fn ->
+      V.validate!(plan, report_all_errors: true)
+    end
+
   end
 
 end
